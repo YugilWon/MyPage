56d4d8c366184f3790808147090a9f9800fc8882
YugilWon <100914677+YugilWon@users.noreply.github.com>
2023-04-19 20:03:10 +0900

    Add files via upload
    05.FanPage Using elasticbeanstalk

---
+++ 05.FanPage/application.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+application = app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://yugil3150:yugilgod1!@yugil.xykmp3l.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbyugil
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/guestbook", methods=["POST"])
+def guestbook_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name' : name_receive,
+        'comment' : comment_receive
+    }
+    db.fans.insert_one(doc)
+
+    return jsonify({'msg': '저장완료!'})
+
+@app.route("/guestbook", methods=["GET"])
+def guestbook_get():
+    all_comments = list(db.fans.find({},{'_id':False}))
+    return jsonify({'result': all_comments})
+
+if __name__ == '__main__':
+    app.run()
